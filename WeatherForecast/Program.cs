07b89cb378d74d5cc0ef00783a00b810de2932ab
Nolan Martin <nmartinsdtc@gmail.com>
2023-04-10 13:00:18 -0600

    prompted user for zipcode and returned current weather in degrees fahrenheit

---
+++ WeatherForecast/Program.cs
@@ -0,0 +1,19 @@
+﻿//using static System.Net.WebRequestMethods;
+
+
+using Newtonsoft.Json.Linq;
+
+string key = File.ReadAllText("appsettings.json");
+string APIkey = JObject.Parse(key).GetValue("DefaultKey").ToString();
+
+Console.WriteLine("Please enter your zip code: ");
+var zip = Console.ReadLine();
+
+var client = new HttpClient();
+var weatherURL = $"https://api.openweathermap.org/data/2.5/weather?zip={zip}&appID={APIkey}&units=imperial";
+var weatherResponse = client.GetStringAsync(weatherURL).Result;
+var formattedResponse = JObject.Parse(weatherResponse).GetValue("main").ToString();
+var tempResponse = JObject.Parse(formattedResponse).GetValue("temp");
+Console.WriteLine($"The current temperature is: {tempResponse} degrees Fahrenheit");
+
+
